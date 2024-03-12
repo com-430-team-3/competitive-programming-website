110bf4f9f7ba2593bf8eabf9169e955e29e8fed7
Bakyt Yrysov <yrysov_b@auca.kg>
2024-03-12 14:06:28 +0600

    Fix logic errors

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule LinkLangClassifier.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.3"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
